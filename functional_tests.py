e6fa29e843799d02949b8ef1fb06e2a69f9ab471
Jess Tucker <jess.tucker@gmail.com>
2017-06-06 08:58:56 -0700

    Basic view now returns html, but I dont see it in browser

--- functional_tests.py
+++ functional_tests.py
@@ -7,7 +7,7 @@ class newVisitorTest(unittest.TestCase):
 		self.browser.implicitly_wait(3)
 	def tearDown(self):
 		self.browser.quit()
-	def test_can_start_a_list_and_retrive_it_later(self):
+	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith has heard about a cool new online to-do app. She goes
 		# to check out its homepage
 		self.browser.get('http://localhost:8000')
