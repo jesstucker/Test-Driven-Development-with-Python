a619a2c6e9d6ac2170994379fa556d96a31f2146
Jess Tucker <jess.tucker@gmail.com>
2017-06-07 10:18:50 -0700

    refactor to include input checker helper function

--- functional_tests.py
+++ functional_tests.py
@@ -39,6 +39,13 @@ class newVisitorTest(unittest.TestCase):
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
 		
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
 
 
 
