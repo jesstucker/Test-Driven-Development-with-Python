fd0fc3493db56fb2f4414fddb6a8bf370475e4d0
Jess Tucker <jess.tucker@gmail.com>
2017-06-07 10:04:07 -0700

    refactor to include custom input

--- functional_tests.py
+++ functional_tests.py
@@ -30,13 +30,17 @@ class newVisitorTest(unittest.TestCase):
 		# "1: Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
 
+		# import time
+		# time.sleep(10)
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows)
-		)
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
+		self.assertIn(
+			'2: Use peacock featers to make a fly',
+			[row.text for row in rows]
+		)
 		self.fail('Finish the test!')
 		# The page updates again, and now shows both items on her list
 
