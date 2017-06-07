4394af6f63003ae71aade4819aa46567f0b91b66
Jess Tucker <jess.tucker@gmail.com>
2017-06-07 13:41:47 -0700

    make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,8 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
-class newVisitorTest(unittest.TestCase):
+class newVisitorTest(LiveServerTestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
@@ -18,7 +18,7 @@ class newVisitorTest(unittest.TestCase):
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith has heard about a cool new online to-do app. She goes
 		# to check out its homepage
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 		# She notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
 		
@@ -30,18 +30,23 @@ class newVisitorTest(unittest.TestCase):
 		)
 		# She types "Buy peacock feathers" into a text box (Edith's hobby
 		# is tying fly-fishing lures)
-		inputbox.send_keys('1: Buy peacock feathers')
+		inputbox.send_keys('Buy peacock feathers')
 		# When she hits enter, the page updates, and now the page lists
 		# "1: Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
+		import time
+		time.sleep(5)
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
-		
+
 		inputbox = self.browser.find_element_by_id('id_new_item')
+		time.sleep(1)
 		inputbox.send_keys('Use peacock feathers to make a fly')
+		time.sleep(1)
 		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
 
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
@@ -61,5 +66,3 @@ class newVisitorTest(unittest.TestCase):
 		# Satisfied, she goes back to sleep
 
 
-if __name__ == '__main__':
-	unittest.main(warnings='ignore')
