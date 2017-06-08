f326d2eea45af38358c5778b05441444cada413e
Jess Tucker <jess.tucker@gmail.com>
2017-06-08 16:11:18 -0700

    ch6 complete

---
+++ lists/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+from lists import views
+
+urlpatterns = patterns('',
+	url(r'^new$', views.new_list, name='new_list'),
+	url(r'(\d+)/$', views.view_list, name='view_list'),
+	url(r'^(\d+)/add_item$', views.add_item, name='add_item'),
+)
