9f806a6acf88248736b9c16e9eab71c8af731377
Jess Tucker <jess.tucker@gmail.com>
2017-06-08 23:33:11 -0700

    set STATIC_ROOT in settings and disable admin

--- superlists/settings.py
+++ superlists/settings.py
@@ -64,7 +64,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -124,7 +124,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'lists',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
